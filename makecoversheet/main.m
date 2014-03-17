fdaaebae8643626f4929de437d5346e4a4a8f1c1
Kevin Meaney <ktam@yvs.eu.com>
2014-03-17 11:20:04 +0000

    Initial Commit

---
+++ makecoversheet/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  makecoversheet
+//
+//  Created by Kevin Meaney on 17/03/2014.
+//  Copyright (c) 2014 Kevin Meaney. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
